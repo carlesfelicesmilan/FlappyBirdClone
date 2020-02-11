452c9b952fa0a55be9c040d553e0b4e6982d9b3a
Carles <carles.felices@gmail.com>
2020-02-11 15:18:40 +0100

    Fixing git issues

---
+++ app/src/main/java/com/example/flappybirdclone/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.flappybirdclone
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_main)
+    }
+}
