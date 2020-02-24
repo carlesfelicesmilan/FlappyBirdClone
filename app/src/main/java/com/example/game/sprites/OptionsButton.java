f430f015ce925390214978819bc6e8c712525fb6
Carles Felices <carles.felices@netcentric.biz>
2020-02-24 15:42:24 +0100

    Added button

--- app/src/main/java/com/example/game/sprites/OptionsButton.java
+++ app/src/main/java/com/example/game/sprites/OptionsButton.java
@@ -4,11 +4,13 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.game.GameManagerCallback;
 import com.example.game.R;
 
-public class Button implements Sprite{
+public class OptionsButton implements Sprite{
 
     private Bitmap optionsButton;
     private int screenHeight, screenWidth;
@@ -17,15 +19,16 @@ public class Button implements Sprite{
 
     private int buttonX, buttonY, buttonWidth, buttonHeight;
 
-    public Button(Resources resources, int screenHeight, int screenWidth) {
+    public OptionsButton(Resources resources, int screenHeight, int screenWidth) {
         this.screenHeight = screenHeight;
         this.screenWidth = screenWidth;
 
         buttonX = (int) resources.getDimension(R.dimen.button_x);
+        buttonY = (int) resources.getDimension(R.dimen.button_y);
         buttonWidth = (int) resources.getDimension(R.dimen.button_width);
         buttonHeight = (int) resources.getDimension(R.dimen.button_height);
 
-        Bitmap buttonOptions = BitmapFactory.decodeResource(resources, R.drawable.dani);
+        Bitmap buttonOptions = BitmapFactory.decodeResource(resources, R.drawable.bird_down);
         optionsButton = Bitmap.createScaledBitmap(buttonOptions, buttonWidth, buttonHeight, false);
 
         optionsClicked = false;
@@ -34,7 +37,9 @@ public class Button implements Sprite{
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawBitmap(optionsButton, screenWidth/2 - optionsButton.getWidth()/2, screenHeight/8, null);
+        if (!optionsClicked) {
+            canvas.drawBitmap(optionsButton, buttonX, buttonY, null);
+        }
     }
 
     @Override
@@ -45,5 +50,18 @@ public class Button implements Sprite{
     public void optionsClicked () {
         optionsClicked = true;
     }
+
+    public boolean isButtonClicked(int x, int y) {
+        if (x >= buttonX && x < (buttonX + buttonWidth)
+                && y >= buttonY && y < (buttonY + buttonHeight)) {
+            //tada, if this is true, you've started your click inside your bitmap
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
 
 }
